c83b4c7d664b07232dabcf8e13eb575d8b8e338c
Christopher Harrison <geektrainer@github.com>
2024-04-05 15:43:11 -0700

    Added Python app

---
+++ checkpoint-tests.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+# Connect to the SQLite database (or create it if it doesn't exist)
+conn = sqlite3.connect('./prisma/dev.db')
+
+# Create a cursor object
+c = conn.cursor()
+
+name = input('Enter name of race: ')
+
+# Execute a query
+c.execute("SELECT * FROM Race WHERE name = '" + name + "'")
+
+# Fetch all the rows
+rows = c.fetchall()
+
+for row in rows:
+    print(row)
+
+# Close the connection
+conn.close()
