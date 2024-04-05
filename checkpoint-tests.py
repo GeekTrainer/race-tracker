65c3fd8912717305ed8e9a3228226b4754964e6b
Christopher Harrison <geektrainer@github.com>
2024-04-05 15:48:09 -0700

    Added query

--- checkpoint-tests.py
+++ checkpoint-tests.py
@@ -8,6 +8,8 @@ c = conn.cursor()
 
 name = input('Enter name of race: ')
 
+# Execute a query
+c.execute("SELECT * FROM Race WHERE name = '" + name + "'")
 
 # Fetch all the rows
 rows = c.fetchall()
